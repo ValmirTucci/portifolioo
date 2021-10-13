2faf4529e5ef033ab3a965f2f90cee6b883ed985
Valmir Izzo <valmirizzo@gmaail.com>
2021-10-13 14:10:02 -0300

    :package: menu sidebar

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var menu = document.querySelector(".menu");
 
 menu.addEventListener("click", function(){
 
-    var sidebar = document.querySelector (".container").classList.toggle("show-menu")
+   document.querySelector (".container").classList.toggle("show-menu")
 });
